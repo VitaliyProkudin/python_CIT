dac73f43916858a8a706f53d14c0d27dfac45945
VitaliyProkudin <vitaliy.prokudin@gmail.com>
2018-06-19 13:33:07 +0300

    ok

---
+++ 1.py
@@ -0,0 +1,17 @@
+msg = "1231356"
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+
+merg?
+
+print(msg)
+print(msg)
+print(msg)
+print(msg)
+print(msg)DEBUG_SAVEALL
