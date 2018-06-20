63552c68af513df4116debb5207f46000812cd47
VitaliyProkudin <vitaliy.prokudin@gmail.com>
2018-06-20 12:51:32 +0300

    true

--- Task1.py
+++ Task1.py
@@ -10,9 +10,9 @@ desc = ((b**2) - 4*a*c)
 if desc < 0:
     print("D<0 Корней нет - введите другие аргументы")
 elif desc == 0:
-        squar = -(b/2*a)
-        print('D=0 2 корня x1=', squar,'x2=', -squar)
+    squar = -(b/2*a)
+    print('D=0 2 корня x1=', squar,'x2=', -squar)
 else:
-        squar_1  = -b+math.sqrt((b**2) - 4*a*c)/2*a
-        squar_2 = -b-math.sqrt((b**2) - 4*a*c)/2*a
-        print('D>0 c округлением  x1=', round(squar_1),'x2=', round(-squar_2))
+    squar_1  = -b+math.sqrt((b**2) - 4*a*c)/2*a
+    squar_2 = -b-math.sqrt((b**2) - 4*a*c)/2*a
+    print('D>0 c округлением  x1=', round(squar_1),'x2=', round(-squar_2))
