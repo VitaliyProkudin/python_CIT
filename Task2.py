314b9303d8ecc36fc123ae75bf3c481687614a3b
VitaliyProkudin <vitaliy.prokudin@gmail.com>
2018-06-21 11:05:40 +0300

    push

--- Task2.py
+++ Task2.py
@@ -2,13 +2,12 @@ import sys
 
 nun_steps = int(sys.argv[1])
 
-
 for x in range(nun_steps): 
       for i in range((nun_steps)-x):
          print(' ',end='')
       for y in range((x)+1):
          print('#',end='')
       print(end='\n')
-    
+
 
 
